6f019355edb49e45fbe6e090fc910c84fab93e6e
esraasobhy2172004@gmail.com <esraasobhy2172004@gmail.com>
2025-09-21 03:00:30 +0300

    Add project files.

---
+++ ConsoleApp9/Program.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("enter your num");
+        int x = int .Parse(Console.ReadLine());
+        Console.WriteLine(x);
+    }
+}
